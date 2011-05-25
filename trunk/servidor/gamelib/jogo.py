7a759ca820ff2e9f13d2daa3df17ec2d158d0386
wanderjardim@gmail.com <wanderjardim@gmail.com@b565e03a-55b1-6899-01c2-d97a4f35e0bf>
2011-05-25 17:17:08 +0000

    Definição da rotina de receber comandos do cliente

--- trunk/servidor/gamelib/jogo.py
+++ trunk/servidor/gamelib/jogo.py
@@ -32,62 +32,30 @@ import time
 
 
 class Jogo(cocos.layer.Layer, EventDispatcher): # must be layer - scene causes anims/actions to fail
-    eh_manipuador_eventos = True # for actions to work
-
-    def __init__(self, jogadores): #, tipo_conexao):
+    def __init__(self, jogadores):
         super(Jogo, self).__init__()
         self.nrJogadores=0
         self.partidaIniciada = False
         self.jogadores = jogadores
-
-
-
-
         # Configurando camadas
         # HUD
         self.hud = Hud(self)
-
         game_audio.next_song()
-        
         self.add(self.hud, z=-1)
-
-
         self.do(Delay(3) +CallFunc(self.dispatch_event,'on_game_start') )
-        
         game_audio.play_song('music_background1.ogg')
-            
-        
-        #jogadoresAtivos = [x for x in self.jogadores if x.isAlive() == False]
-        
         if self.jogadores == []:
             print "Jogadores veio vazio!"
         else:
-            qtdJogadoresAtivos = len(self.jogadores)
-            print "tamanho jogadores:"
-            print qtdJogadoresAtivos
             for i in self.jogadores:
-                print i
-                print i.nome
-                print i.socket
                 th = Thread(target=self.aguarda_comando, args=(i,))
                 th.setDaemon(True)
                 th.start()
                 i.envia_comando("Bem Vindo ao Jogo!!")
-                
-        """
-        qtdJogadoresAtivos = len(self.jogadores)
+                time.sleep(4)
+                i.envia_comando("iniciaPartida")
         print "Vai começar a recepção:"
-        
-        th=Thread( target=self.aguarda_comando, args = ( qtdJogadoresAtivos, ))
-        th.setDaemon(True)
-        th.start()
-        #Thread(self.aguarda_comando()).start()
-        """
-        
-        
-        
-        
-        
+   
         
     def aguarda_comando(self, jogador):
         i = 0
@@ -104,53 +72,12 @@ class Jogo(cocos.layer.Layer, EventDispatcher): # must be layer - scene causes a
             else:
                 print "Recebi isso mesmo: "
                 print msg
-        
-        
         jogador.desconecta()
-        self.hud.desconectaJogador()
+        self.hud.desconectaJogador(jogador.nome)
         #director.pop()
         #director.pop()
         #ou
-        director.scene.end()
-        
-
-        """
-        
-        print "Vai começar a recepção:"
-        """
-        
-        """
-        while True:
-            msg = self.jogador_sock.recv(1024)
-            if msg == "sair":
-                break
-            else:
-                print msg
-                self.jogadores[1].envia_comando(msg)
-                
-        
-        
-        """
-        #for j in self.jogadores:
-        #    j.desconecta()
-        
-        
-        
-
-
-
-    def conecta_jogadoresBT(self):
-        self.nrJogador = 0
-        while not self.partidaIniciada:
-            print "Rodei pela %sa. vez" % self.nrJogador
-            self.jogadores.append(JogadorBT(self.conexao, self.hud, self.nrJogador))
-            self.jogadores[self.nrJogador].setDaemon(True)
-            self.jogadores[self.nrJogador].start()
-            self.nrJogador+=1
-            
-            if len(self.jogadores)>=2:
-                self.partidaIniciada = True
-        self.nrJogadores = len(self.jogadores)
+        #director.scene.end()
         
     def on_quit(self):
         # called by esc
@@ -167,24 +94,16 @@ class ComandosJogador(threading.Thread):
         self.jogador.estahRodando = True
         print self.jogador.recebe_comando()
 
-
-
 Jogo.register_event_type('on_game_start')   
 Jogo.register_event_type('on_xp_gain')   
 Jogo.register_event_type('on_game_over')
 Jogo.register_event_type('on_gamer_connect')
 Jogo.register_event_type('on_server_status')
 
-
 def run(jogadores): #tipo_conexao):
-
-    
     return Jogo(jogadores) #tipo_conexao)
 
 if __name__ == '__main__':
 
-
-
     director.init(width=640, height=480, do_not_scale=True)    
     director.run(run())
-
