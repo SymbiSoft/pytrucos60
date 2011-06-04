827e4912144d2eb597a62377a57f84e619af0f08
wanderjardim@gmail.com <wanderjardim@gmail.com@b565e03a-55b1-6899-01c2-d97a4f35e0bf>
2011-06-04 19:29:14 +0000

    Inclusão dos novos arquivos da biblioteca

---
+++ trunk/cliente/gamelib/constantes.py
@@ -0,0 +1,24 @@
+#-*- coding: utf-8 -*-
+
+"""Definição das constantes.
+
+Este arquivo é carregado durante a inicialização do programa antes de o módulo 
+principal ser importado. Por conseguinte, este arquivo não deve importar qualquer 
+outro módulo, ou os módulos serão inicializados antes do módulo principal, o 
+que significa que o valor DEBUG pode não ter sido configurado corretamente.
+
+Este módulo destina-se a ser importado com a semântica 'from ... import *', mas
+ele não fornece uma especificação __all__.
+
+"""
+
+#Definições das constantes de cores
+RGB_LIGHT_BLUE = (65, 156, 241)
+
+RGB_RED = (255, 0, 0)
+RGB_GREEN = (0, 255, 0)
+RGB_BLUE = (0, 0, 255)
+RGB_PURPLE = (100,0,255)
+RGB_BLACK = (0,0,0)
+
+
