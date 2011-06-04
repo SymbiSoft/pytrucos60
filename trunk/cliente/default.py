55895e0fe73985edb870407741a7d9a9a4df04f3
wanderjardim@gmail.com <wanderjardim@gmail.com@b565e03a-55b1-6899-01c2-d97a4f35e0bf>
2011-06-04 19:38:41 +0000

    Exclusão de arquivos não necessários

--- trunk/cliente/default.py
+++ trunk/cliente/default.py
@@ -30,7 +30,9 @@ import e32
 import sys
 import os
 
-#PYTRUCOS60_UID = u"ef080808"
+
+
+PYTRUCOS60_UID = u"ef080808"
 
 PYTRUCOS60_PATH1 = "\\Python\\"
 PYTRUCOS60_PATH2 = "\\Data\\python\\"
@@ -58,42 +60,33 @@ def get_path(app_name):
             return os.path.join(drive, PYTRUCOS60_PATH1)
         elif os.path.isfile(os.path.join(drive, PYTRUCOS60_PATH2, app_name)):
             return os.path.join(drive, PYTRUCOS60_PATH2)
-#        elif os.path.isfile(os.path.join(drive, PYTRUCOS60_PATH3, app_name)):
-#            return os.path.join(drive, PYTRUCOS60_PATH3)
+        elif os.path.isfile(os.path.join(drive, PYTRUCOS60_PATH3, app_name)):
+            return os.path.join(drive, PYTRUCOS60_PATH3)
     return None
 
 
 full_path = get_path('default.py') 
 if full_path == None:
-    full_path = get_path('default.py')
+    full_path = get_path('run_game.py')
     if full_path == None:
         appuifw.note(u'Usando fallback path')
         full_path = u'C:\\' + u'Private' + u'\\' + PYTRUCOS60_UID + u'\\'
 
 
-sys.path.append(os.path.join(full_path, "pytruco","libs"))
+sys.path.append(os.path.join(full_path, "gamelib"))
 
 
+from Pytrucos60 import Pytrucos60
 
-from game import *
 
-
-app = Game(os.path.join(full_path, "pytruco"))
+app = Pytrucos60()
 
 
 timer = e32.Ao_timer()
-
 flag = 1
 while flag:
-    if (app.running == 1):
-        escancode_pressed = app.pressablekeys_pressed()
-        if (escancode_pressed):
-            timer.cancel()
-            app.pressablekeys_process(escancode_pressed)
-            timer.after(1.5, pressablekeys_followup)
-        else:
-            app.otherkeys_process()
-    elif app.running == -1:
+    if app.running == -1:
         flag = 0
     e32.ao_yield()
+
 
