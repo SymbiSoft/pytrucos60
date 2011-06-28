a7a1bfc8b24768c38339386305b34357b5948d7e
wanderjardim@gmail.com <wanderjardim@gmail.com@b565e03a-55b1-6899-01c2-d97a4f35e0bf>
2011-06-28 19:45:50 +0000

    Novas alterações no cliente

--- trunk/cliente/gamelib/splash.py
+++ trunk/cliente/gamelib/splash.py
@@ -9,39 +9,15 @@
 # 
 #######################################################
 
-
 from graphics import Image
-import e32
-import os
-import sysinfo
-
-IMG_PATH1 = "\\Python\\img\\"
-IMG_PATH2 = "\\Data\\python\\img\\"
-IMG_PATH3 = "\\Private\\ef0b4099\\img\\"
+import util
 
 class Splash:
     
     def __init__ (self):
-        self.largura_tela, self.altura_tela = self.__getTamanho_tela()
+        self.largura_tela, self.altura_tela = util.getTamanho_tela()
+        self.fundoSplash=util.cria_imagem('splash.png')
         
-        self.__caregar_imagens()
-    
-    def __caregar_imagens(self):
-        caminho_img = self.__get_caminho('splash.png')
-        
-        self.fundoSplash=self.__cria_imagem(caminho_img + 'splash.png')
-        
-
-    def __cria_imagem(self, caminho):
-        """Carrega imagem referente ao parametro passado. Se a imagem não existir no
-        diretório, retorna vazio.
-        """
-        try:
-            img = Image.open(caminho)
-            return img
-        except:
-            print "Imagem não encontrada!"
-            return None
 
 
     def desenha_splash(self, canvas):
@@ -54,22 +30,3 @@ class Splash:
             tela_splash.clear((fond,fond,fond))
             tela_splash.blit(self.fundoSplash,mask=mascaraSplash)
             canvas.blit(tela_splash) # mostra a imagem na tela
-            #e32.ao_sleep(0.06)
-        #e32.ao_sleep(0.5)
-
-
-
-    def __get_caminho(self, arquivo):
-        drives_list = e32.drive_list()
-        drives_list.reverse()
-        for drive in [str(x) for x in drives_list]:
-            if os.path.isfile(os.path.join(drive, IMG_PATH1, arquivo)):
-                return os.path.join(drive, IMG_PATH1)
-            elif os.path.isfile(os.path.join(drive, IMG_PATH2, arquivo)):
-                return os.path.join(drive, IMG_PATH2)
-            elif os.path.isfile(os.path.join(drive, IMG_PATH3, arquivo)):
-                return os.path.join(drive, IMG_PATH3)
-        return None
-
-    def __getTamanho_tela(self):
-        return sysinfo.display_pixels()
